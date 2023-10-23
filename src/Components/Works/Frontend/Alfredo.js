a30da2299fdb9f8bce67a1270dc850ae0f1f277e
Bootstrap02 <udegbue69@gmail.com>
2023-10-23 17:13:56 +0100

    first commit

---
+++ src/Components/Works/Frontend/Alfredo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alfredo = () => {
+  return (
+    <div>Alfredo</div>
+  )
+}
+
+export default Alfredo
---
+++ src/Components/Works/Frontend/Cchomes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cchomes = () => {
+  return (
+    <div>Cchomes</div>
+  )
+}
+
+export default Cchomes
